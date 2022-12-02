a055ee022f76b5afe83b231eb56526201750d39e
KDouble <kdouble@t-online.de>
2022-12-02 20:40:07 +0100

    add day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -17,7 +17,7 @@ pub fn solve_part1(input: &str) -> i32 {
   for s in input.split("\n") {
     let elf = s.chars().nth(0).unwrap();
     let player = s.chars().nth(2).unwrap();
-    
+
     score += RPSMAP.get(&player).unwrap();
 
     match wrap(RPSMAP.get(&player).unwrap() - RPSMAP.get(&elf).unwrap()) {
@@ -40,4 +40,37 @@ fn wrap(input: i32) -> i32 {
   }
 
   return input;
+}
+
+#[aoc(day2, part2, Char)]
+pub fn solve_part2(input: &str) -> i32 {
+  let mut score = 0;
+
+  for s in input.split("\n") {
+    let elf = s.chars().nth(0).unwrap();
+    let condition = s.chars().nth(2).unwrap();
+
+    let elf_value = RPSMAP.get(&elf).unwrap(); 
+    match condition {
+      'X' => {
+        let mut value = ((elf_value - 2) % 3 )+ 1;
+        if value <= 0 {
+          value += 3
+        }
+        score += value;
+      },
+      'Y' => {
+        score += 3;
+        score += elf_value;
+      },
+      'Z' => {
+        score += 6;
+        score += elf_value % 3 + 1;
+      },
+      _ => panic!(),
+    }
+
+  }
+
+  return score
 }
