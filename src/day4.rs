95e0240840ad724647ae860bed7b37ecfed63599
KDouble <kdouble@t-online.de>
2022-12-04 19:28:45 +0100

    add day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -26,6 +26,29 @@ fn to_string(input: &str) -> String {
   return result
 }
 
-// #[aoc(day4, part2, Char)]
-// pub fn solve_part2(input: &str) -> i32 {
-// }
+fn to_array(input: &str) -> Vec<i32> {
+  let start: i32 = input.split("-").nth(0).unwrap().parse().unwrap();
+  let end: i32 = input.split("-").nth(1).unwrap().parse().unwrap();
+
+  let result: Vec<i32> = (start..=end).collect();
+
+  return result
+}
+
+#[aoc(day4, part2, Char)]
+pub fn solve_part2(input: &str) -> i32 {
+  let mut result = 0;
+  for s in input.split("\n"){
+    let a = to_array(s.split(",").nth(0).unwrap());
+    let b = to_array(s.split(",").nth(1).unwrap());
+    
+    for ax in a {
+      if b.contains(&ax) {
+        result += 1;
+        break;
+      }
+    }
+  }
+  
+  return result
+}
