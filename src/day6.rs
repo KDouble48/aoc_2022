1db95047c2710864aef8ed2ccac51d4ea002804a
KDouble <kdouble@t-online.de>
2022-12-06 18:33:37 +0100

    add day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -2,21 +2,25 @@ use std::collections::HashSet;
 
 #[aoc(day6, part1, Char)]
 pub fn solve_part1(input: &str) -> usize {
+  return solve(input, 4);
+}
+
+fn solve(input: &str, len: usize) -> usize {
   for x in 0..input.len() {
-    let values = &mut input[x..x+4].chars().collect::<Vec<char>>();
+    let values = &mut input[x..x+len].chars().collect::<Vec<char>>();
 
     let mut uniques = HashSet::new();
     values.retain(|v| uniques.insert(v.clone()));
 
-    if values.len() == 4 {
-      return x+4;
+    if values.len() == len {
+      return x+len;
     }
   }
 
   return 0;
 }
 
-// #[aoc(day6, part2, Char)]
-// pub fn solve_part2(input: &str) -> i32 {
-
-// }
+#[aoc(day6, part2, Char)]
+pub fn solve_part2(input: &str) -> usize {
+  return solve(input, 14);
+}
